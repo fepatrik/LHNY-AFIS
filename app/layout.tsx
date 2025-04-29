b28e8982fb22f3742bd3f2246677ef6a94d2c184
fepatrik <fepatrik@windowslive.com>
2025-04-29 09:22:57 +0200

    3

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
           minHeight: "100vh",
           margin: "0",
           padding: "20px",
-          backgroundImage: `url("https://img.freepik.com/free-photo/abstract-gradient-background-with-grainy-effect_84443-3113.jpg?t=st=1744361142~exp=1744364742~hmac=97756c5c6e5461008f9952f15c9da5c3fdfc02fa00b617fc43f3ea7ac4bd04d1&w=1380")`,
+          backgroundImage: `url("/bg.jpg")`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
